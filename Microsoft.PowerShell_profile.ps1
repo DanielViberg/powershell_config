fa616f9147089f1aa2260c43545aadf9a69b7931
DanielViberg <daniel_viberg@outlook.com>
2021-11-10 15:45:16 +0100

    modified

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 clear
-oh-my-posh --init --shell pwsh --config ./daniel.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config $HOME\Documents\Powershell\daniel.omp.json | Invoke-Expression
